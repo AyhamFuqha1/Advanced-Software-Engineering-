41e72f6003c81a107c303ca862248755dc017999
AyhamFuqha1 <ayhamfuqha55@gmail.com>
2025-11-29 13:30:23 +0200

    save changes before switching

--- src/donors/donors.service.ts
+++ src/donors/donors.service.ts
@@ -1,26 +1,54 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { Donor } from './entities/donor.entity';
 import { CreateDonorDto } from './dto/create-donor.dto';
 import { UpdateDonorDto } from './dto/update-donor.dto';
 
 @Injectable()
 export class DonorsService {
-  create(createDonorDto: CreateDonorDto) {
-    return 'This action adds a new donor';
+  constructor(
+    @InjectRepository(Donor)
+    private readonly donorsRepository: Repository<Donor>,
+  ) {}
+
+  
+  async create(createDonorDto: CreateDonorDto) {
+    const donor = this.donorsRepository.create(createDonorDto);
+    return await this.donorsRepository.save(donor);
   }
 
-  findAll() {
-    return `This action returns all donors`;
+  
+  async findAll() {
+    return await this.donorsRepository.find();
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} donor`;
+ 
+  async findOne(id: number) {
+    const donor = await this.donorsRepository.findOne({
+      where: { id_donors: id },
+    });
+
+    if (!donor) {
+      throw new NotFoundException(`Donor #${id} not found`);
+    }
+
+    return donor;
   }
 
-  update(id: number, updateDonorDto: UpdateDonorDto) {
-    return `This action updates a #${id} donor`;
+
+  async update(id: number, updateDonorDto: UpdateDonorDto) {
+    const donor = await this.findOne(id);
+
+    const updated = Object.assign(donor, updateDonorDto);
+    return await this.donorsRepository.save(updated);
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} donor`;
+
+  async remove(id: number) {
+    await this.findOne(id);
+
+    await this.donorsRepository.delete(id);
+    return { message: `Donor #${id} deleted successfully` };
   }
 }
